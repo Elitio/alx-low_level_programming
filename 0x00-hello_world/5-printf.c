9f199901d85a46374496820180b62499e0119bfb
Christian Annan Segbenu <elitio33@gmail.com>
2023-02-16 16:54:53 -0800

    Write a C program that prints exactly characters with new line using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
+
 /**
  * main - This is the main function
  *
  * Return: Should return just 0
  */
 
-int main (voi) 
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n);
 	return (0);
