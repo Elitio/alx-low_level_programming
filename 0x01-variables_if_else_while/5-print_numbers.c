09ebeadb3bc411becc33bbcd1a7613aa263f71b2
Christian Annan Segbenu <elitio33@gmail.com>
2023-02-17 09:12:08 -0800

    Print numbers from 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - this is the part of the code that executes firts
+ *
+ * Return: the return value is 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
