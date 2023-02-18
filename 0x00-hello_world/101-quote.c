1c6f9ed5867ad24e8f0da5be37834b197c042350
Christian Annan Segbenu <elitio33@gmail.com>
2023-02-18 04:23:06 -0800

    Write a C program that prints text, followed by a new line, to the standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <time.h>
+#include <string.h>
 
 /**
  * main - This is the code that executes first
  *
- * Return: Returns 0 for success
+ * Return: Returns 1 for success
  */
 
 int main(void)
 {
 	char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(2, str, sizeof(str) - 1);
-	return (0);
+	write(2, str, strlen(str));
+	return (1);
 }
