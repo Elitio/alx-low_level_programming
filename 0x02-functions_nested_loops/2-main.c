799308f1465f95bb126477a1bf1c2cea90ea3e9a
Christian Annan Segbenu <elitio33@gmail.com>
2023-02-22 12:47:28 -0800

    Write a function that prints 10 times the alphabet, in lowercase, followed by a new line using prototype function

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - This is the block of code that gets executed first
+ *
+ * Return: Always returns 0 when successful
+ */
+
+int main(void)
+{
+	print_alphabet_x10();
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - This is the prototype that prints a to z x10
+ *
+ * Return: This returns 0 for success
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+	}
+	_putchar('\n');
+}
