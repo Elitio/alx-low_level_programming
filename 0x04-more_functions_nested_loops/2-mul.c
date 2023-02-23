e55e8cc2687cde54c6742c087e228af8db5fa582
Christian Annan Segbenu <elitio33@gmail.com>
2023-02-23 13:23:41 -0800

    Write a function that multiplies two integers using prototype function

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - This is the prototype function to multiply two numbers
+ *
+ * @a: This is the first parameter
+ * @b: This is the second parameter
+ *
+ * Return: Always return 0
+ */
+
+int mul(int a, int b)
+{
+	int m;
+
+	m = a + b;
+	return (m);
+}
