44055847c75b76bc5913352291eb26161b293fc5
Christian Annan Segbenu <elitio33@gmail.com>
2023-02-23 07:48:32 -0800

    Write a function that checks for uppercase character using prototype function

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+
+#define MAIN_H
+
+/**
+ * file - This is a main.h file
+ *
+ * Auther: Christian Annan Segbenu (Elitio)
+ * Desc: All prototypes used here
+ */
+
+int _putchar(char c);
+int _isupper(int c);
+
+#endif
