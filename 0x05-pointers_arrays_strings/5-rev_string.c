db7ffdbcf4cb54fec4ea86f01749afdd2dff02ac
Christian Annan Segbenu <elitio33@gmail.com>
2023-02-27 13:42:44 -0800

    Write a function that reverses a string using prototpye

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * rev_string - This is the prototype function that reverses string
+ *
+ * @s: This is the parameter
+ * Return: This will return 0 when successful
+ */
+
+void rev_string(char *s)
+{
+	int len = strlen(s);
+	int i;
+
+	for (i = len - 1; i >= 0; i--)
+	{
+		printf("%c", s[i]);
+	}
+	putchar('\n');
+}
