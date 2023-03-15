4d56d9480e5dd9dd5b96a0f798c5ce5541a30258
Christian Annan Segbenu <elitio33@gmail.com>
2023-03-15 06:22:55 -0700

    Write a function that concatenates all the arguments of your program

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -25,10 +25,9 @@ char *argstostr(int ac, char **av)
 	for (i = 0; i < ac; i++)
 	{
 		total_length += strlen(av[i]) + 1; /* +1 for the newline character */
-		total_length++;
 	}
 
-	result = malloc(total_length * sizeof(char));
+	result = malloc((total_length + 1) * sizeof(char));
 
 	if (result == NULL)
 	{
