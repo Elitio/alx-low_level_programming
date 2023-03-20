4b4bf7a99c8b0f6ac6bf222aa8a4bcc5c145494f
Christian Annan Segbenu <elitio33@gmail.com>
2023-03-20 15:04:34 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 02-main.c -o cc

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - This is the main function
+ *
+ * @void: Will return void
+ * Return: Will return 0 on success
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
