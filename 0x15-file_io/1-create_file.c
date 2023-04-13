976cf1d9dc2f6814af4a80e089c4e946055aa4ea
Christian Annan Segbenu <elitio33@gmail.com>
2023-04-13 04:39:19 -0700

    Create a function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -34,6 +34,15 @@ int create_file(const char *filename, char *text_content)
 			return (-1);
 		}
 	}
+	else
+	{
+		write_bytes = write(fd, "", 0);
+		if (write_bytes == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
 	close(fd);
 	return (1);
 }
